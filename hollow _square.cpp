de001aaf18985bb6be746128aea94feb2610bd63
Harshita Rawat <harshita.reenu184@gmail.com>
2022-11-25 23:51:49 +0530

    Update hollow _square.cpp
    Program to print a hollow square using printf( ) function.

--- hollow _square.cpp
+++ hollow _square.cpp
@@ -1,15 +1,5 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
     int i,j;
@@ -17,11 +7,6 @@ int main()
     cout<<"*       *\n";
     cout<<"*       *\n";
     cout<<"*       *\n";
-    cout<<"* * * * *\n";
-    
-             
-    
-    
-        
-    return 0;
+    cout<<"* * * * *\n";    
+       return 0;
 }
