dcc425f3484980032af7bd144c296a769c9389d6
Harshita Rawat <harshita.reenu184@gmail.com>
2022-12-25 18:12:28 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    static int id;
+    static int reg_no;
+    int standard;
+    string name;
+    public:
+    student()
+    {
+        standard=1;
+    }
+    ~student()
+    {
+        cout<<"Student id is deleted.";
+    }
+    void print_id()
+    {
+        cout<<"Student id is "<<id<<endl;
+        id++;
+    }
+    void print_reg()
+    {
+        cout<<"Registration number is "<<reg_no<<endl;
+        reg_no++;
+    }
+    void print_name();
+}s1,s2,s3,s4;
+void student:: print_name()
+{
+    cout<<"Enter the name:";
+    cin>>name;
+    cout<<"student name is "<<name<<endl;
+
+}
+int student:: id=1;
+int student:: reg_no=2100;
+int main()
+{
+    s1.print_id();
+    s1.print_name();
+    s1.print_reg();
+    s2.print_id();
+    s2.print_name();
+    s2.print_reg();
+    return 0;
+}
