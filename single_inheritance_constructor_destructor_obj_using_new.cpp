cd9c9b81e2dcc6592e2094e3bf5fd83def0fb3bc
Harshita Rawat <harshita.reenu184@gmail.com>
2023-01-12 01:43:13 +0530

    Initial commit

---
+++ single_inheritance_constructor_destructor_obj_using_new.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class base
+{
+    public:
+        base(){cout <<"I am in base"<<endl;}
+        ~base(){cout<<"Base destructor";}
+        
+};
+
+
+class Derived: public base
+{
+    public:
+        Derived(){cout<<"I am in Derived"<<endl;}
+        ~Derived(){cout<<"Derived destructor"<<endl;}
+};
+
+int main()
+{
+    
+    base *ptr = new Derived; // new operator always call the constructos first
+    
+}
+
+
